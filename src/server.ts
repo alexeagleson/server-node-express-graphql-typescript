908c0b5dc86d65a27eb856085fdd8620d77a5100
Alex Eagleson <alexeagleson@gmail.com>
2019-05-15 17:00:39 -0400

    added initial connection to mongo database

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+'use strict';
+
+require('dotenv').config();
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
@@ -5,6 +8,42 @@ import graphqlHTTP from 'express-graphql';
 import gql from 'graphql-tag';
 import path from 'path';
 import { buildASTSchema } from 'graphql';
+import { Db, MongoClient, AggregationCursor } from 'mongodb';
+
+// Connect using the connection string
+MongoClient.connect(
+  `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${
+    process.env.MONGO_HOST
+  }:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
+  { useNewUrlParser: true },
+  (err, client) => {
+    const autoDb = client.db(process.env.MONGO_DATABASE);
+    const averageScore: AggregationCursor<any> = autoDb
+      .collection('reviews')
+      .aggregate([
+        { $match: { segmentation: 'W2' } },
+        {
+          $group: {
+            _id: null,
+            averageScore: { $avg: '$total_score' },
+            count: { $sum: 1 },
+          },
+        },
+      ]);
+
+    averageScore
+      .next()
+      .then((result: { _id: null; averageScore: number; count: number }) => {
+        console.log(result);
+      });
+
+    setTimeout(() => {
+      client.close().then(() => {
+        console.log('Mongo connection has been closed.');
+      });
+    }, 5000);
+  },
+);
 
 const POSTS = [
   { car: 'Echo', manufacturer: 'Toyota' },
@@ -35,12 +74,16 @@ const schema = buildASTSchema(gql`
   }
 `);
 
-const mapPost = (post: {}, id: number) => post && ({ id, ...post });
+const mapPost = (post: {}, id: number) => post && { id, ...post };
 
 const root = {
   post: ({ id }: any) => mapPost(POSTS[id], id),
   posts: () => POSTS.map(mapPost),
-  submitPost: ({ input: { id, car, manufacturer } }: { input: {id: number, car: string, manufacturer: string }}) => {
+  submitPost: ({
+    input: { id, car, manufacturer },
+  }: {
+    input: { id: number; car: string; manufacturer: string };
+  }) => {
     const post = { car, manufacturer };
     let index = POSTS.length;
 
@@ -59,14 +102,21 @@ const app: express.Application = express();
 
 app.use(helmet());
 app.use(cors());
-app.use('/graphql', graphqlHTTP({
-  graphiql: true,
-  rootValue: root,
-  schema,
-}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    rootValue: root,
+    schema,
+  }),
+);
 
 // Serve the front end automotive application
-const distPath: string = path.join(__dirname, '../../client-automotive', 'build');
+const distPath: string = path.join(
+  __dirname,
+  '../../client-automotive',
+  'build',
+);
 app.use(express.static(distPath));
 
 const port: number = 4000;
@@ -74,3 +124,31 @@ app.listen(port);
 
 /* tslint:disable */
 console.log(`Running a GraphQL API server at localhost:${port}/graphql`);
+
+// setTimeout(() => {
+//   activeMongoClient.close().then(() => {
+//     console.log('Mongo connection has been closed.');
+//   });
+// }, 3000);
+
+// const closeApp = () => {
+//   console.log('Shutting down server.')
+//   // process.exit(2);
+// }
+
+// // do app specific cleaning before exiting
+// process.on('exit', function () {
+//   closeApp();
+// });
+
+// // catch ctrl+c event and exit normally
+// process.on('SIGINT', function () {
+//   closeApp();
+//   // process.exit(2);
+// });
+
+// //catch uncaught exceptions, trace, then exit normally
+// process.on('uncaughtException', function(e) {
+//   closeApp();
+//   // process.exit(99);
+// });
